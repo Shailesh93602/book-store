c9fbb751d7b9dac86428915e216468c6696080a0
Shailesh Chaudhari <shailesh93602@gmail.com>
2023-08-14 14:36:59 +0530

    Add files via upload

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -5,6 +5,10 @@ import Register from "../pages/register";
 import { RoutePaths } from "../utils/enum";
 import { useAuthContext } from "../context/auth";
 import { BookListing } from "../pages/book-listing";
+import Book from "../pages/book";
+import EditBook from "../pages/book/editBook";
+import User from "../pages/user";
+import EditUser from "../pages/user/editUser";
 
 export const MainNavigation = () => {
   const authContext = useAuthContext();
@@ -19,6 +23,33 @@ export const MainNavigation = () => {
         exact
         path={RoutePaths.BookListing}
         element={authContext.user.id ? <BookListing /> : Redirect}
+      />
+
+      <Route
+        exact
+        path={RoutePaths.Book}
+        element={authContext.user.id ? <Book /> : Redirect}
+      />
+      <Route
+        exact
+        path={RoutePaths.EditBook}
+        element={authContext.user.id ? <EditBook /> : Redirect}
+      />
+      <Route
+        exact
+        path={RoutePaths.AddBook}
+        element={authContext.user.id ? <EditBook /> : Redirect}
+      />
+
+      <Route
+        exact
+        path={RoutePaths.User}
+        element={authContext.user.id ? <User /> : Redirect}
+      />
+      <Route
+        exact
+        path={RoutePaths.EditUser}
+        element={authContext.user.id ? <EditUser /> : Redirect}
       />
     </Routes>
   );
